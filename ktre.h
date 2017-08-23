19dacd16028ec9647d51a837945d6c1f4b623284
KrokodileGlue <KrokodileGlue@outlook.com>
2017-08-23 10:48:58 -0700

    ktre: license the project and add a few escape sequences

--- ktre.h
+++ ktre.h
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 "KrokodileGlue" (https://github.com/KrokodileGlue)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #ifndef KTRE_H
 #define KTRE_H
 
@@ -235,13 +259,28 @@ factor(struct ktre *re)
 			left->class = strclone(" \t\r\n\v\f");
 			NEXT;
 			break;
+		case 'S':
+			left->type = NODE_NOT;
+			left->class = strclone(" \t\r\n\v\f");
+			NEXT;
+			break;
 		case 'd':
 			left->type = NODE_CLASS;
 			left->class = strclone("0123456789");
 			NEXT;
 			break;
+		case 'D':
+			left->type = NODE_NOT;
+			left->class = strclone("0123456789");
+			NEXT;
+			break;
 		case 'w':
 			left->type = NODE_CLASS;
+			left->class = strclone("_0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+			NEXT;
+			break;
+		case 'W':
+			left->type = NODE_NOT;
 			left->class = strclone("_0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
 			NEXT;
 			break;
